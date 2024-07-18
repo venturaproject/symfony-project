6e904f3148c9c82424e8fbd69dd1fd2d91ae9eb8
ventura <antonioventura.j@gmail.com>
2024-07-18 09:03:58 +0200

    chore: update content

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -8,4 +8,6 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];

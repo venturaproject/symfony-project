e45eb5a8163e1d301146deee6361fe9787ef1adb
ventura <antonioventura.j@gmail.com>
2024-07-24 15:29:51 +0200

    chore: add new conttroller

---
+++ app/src/Shared/Infrastructure/Controller/HomepageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Shared\Infrastructure\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomepageController extends AbstractController
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    #[Route('/', name: 'app_homepage')]
+    public function index(): Response
+    {
+        // Ejemplo de registro de mensajes de log
+        $this->logger->info('Accediendo a la página de inicio');
+
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+}

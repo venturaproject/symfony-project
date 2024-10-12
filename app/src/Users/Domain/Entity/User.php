b597a3d91813304add487811f97a469be92df31a
ventura <antonioventura.j@gmail.com>
2024-10-12 16:19:03 +0200

    refactor: code review

--- app/src/Users/Domain/Entity/User.php
+++ app/src/Users/Domain/Entity/User.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 
 namespace App\Users\Domain\Entity;
 
@@ -7,12 +6,23 @@ use App\Shared\Domain\Service\UlidService;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+/**
+ * Class User
+ *
+ * This class represents a user in the system.
+ */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    private string $ulid;
-    private string $email;
-    private string $password;
+    private string $ulid; 
+    private string $email; 
+    private string $password; 
 
+    /**
+     * User constructor.
+     *
+     * @param string $email User's email address
+     * @param string $hashedPassword User's hashed password
+     */
     public function __construct(string $email, string $hashedPassword)
     {
         $this->ulid = UlidService::generate();
@@ -20,36 +30,69 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->password = $hashedPassword;
     }
 
+    /**
+     * Get the user's unique identifier.
+     *
+     * @return string
+     */
     public function getUlid(): string
     {
         return $this->ulid;
     }
 
+    /**
+     * Get the user's email address.
+     *
+     * @return string
+     */
     public function getEmail(): string
     {
         return $this->email;
     }
 
+    /**
+     * Get the user's password.
+     *
+     * @return string
+     */
     public function getPassword(): string
     {
         return $this->password;
     }
 
+    /**
+     * Get the username (email) for the user.
+     *
+     * @return string
+     */
     public function getUsername(): string
     {
         return $this->email;
     }
 
+    /**
+     * Get the roles for the user.
+     *
+     * @return array
+     */
     public function getRoles(): array
     {
         return ['ROLE_USER'];
     }
 
+    /**
+     * Erase sensitive credentials.
+     */
     public function eraseCredentials(): void
     {
-        // Implementación opcional: borra las credenciales almacenadas temporalmente, si las hay
+        // Implement logic to erase credentials if necessary.
     }
 
+    /**
+     * Get the user identifier.
+     *
+     * @return string
+     */
     public function getUserIdentifier(): string
     {
         return $this->email;
